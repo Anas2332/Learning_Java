7aa68f6cf3def2e59338b87f608c318f121b2fd9
Anas2332 <amunir2332@gmail.com>
2023-05-01 19:32:34 +0500

    Done with print statements

---
+++ Understanding  Print Statement/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("M.Munir");
+        System.out.println("Shazia Munir");
+        System.out.println("M.Waqas Munir");
+        System.out.println("Malaika Munir");
+        System.out.println("M.Anas Munir");
+    }
+}
